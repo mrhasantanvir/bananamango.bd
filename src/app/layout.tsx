2ae7f3f12c0b10b7a17ace4242c96cd63104b524
mrhasantanvir <smmhasantanvir@gmail.com>
2026-05-09 15:51:51 +1000

    Add Microsoft Clarity tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,6 +73,16 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
+        {/* Microsoft Clarity Code */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wo9khlse2x");
+          `}
+        </Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
